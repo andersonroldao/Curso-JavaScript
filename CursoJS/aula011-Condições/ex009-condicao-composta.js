70d45fe9513244e2cdbc746e5baaf5daa7101780
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-06-19 08:33:00 -0300

    Create ex009-condicao-composta.js

---
+++ CursoJS/aula011-Condições/ex009-condicao-composta.js
@@ -0,0 +1,10 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais=='Brasil' || 'brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
+
+/* Tranformar em Site
+ex: <input type="text" name="ensino" id="ensino" style="text-transform: uppercase"> */
