e3707907d71ac191143514ae44480b5d44c92190
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-06-21 07:16:40 -0300

    Update ex009-condicao-composta.js

--- CursoJS/aula011-Condições/ex009-condicao-composta.js
+++ CursoJS/aula011-Condições/ex009-condicao-composta.js
@@ -5,6 +5,3 @@ if (pais=='Brasil' || 'brasil') {
 } else {
     console.log('Estrangeiro')
 }
-
-/* Tranformar em Site
-ex: <input type="text" name="ensino" id="ensino" style="text-transform: uppercase"> */
