fff70dbfb8663453879d5143ecd9ebeb20e794ed
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-06-28 08:37:04 -0300

    Update for.js

---
+++ CursoJS/aulas/aula014-repeticoes-2/for.js
@@ -0,0 +1,21 @@
+/* // while
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+} */
+
+/* // do while
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5) */
+
+
+// for
+console.log('Vai começar...');
+for (var c = 1; c <= 5; c++) {
+    console.log(c);
+}
+console.log('Fim');
