366b50b05e23b0d771c4d238e344990d4ea74d51
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-07-17 11:09:23 -0300

    Update vetor-na-tela.js

--- CursoJS/aulas/aula015-array-vetor/vetor-na-tela.js
+++ CursoJS/aulas/aula015-array-vetor/vetor-na-tela.js
@@ -2,15 +2,19 @@ let valores = [8, 1, 7, 4, 2, 9]
 
 console.log(valores);
 
-/*
+/* O 3 trechos tem a mesma função
 console.log(valores[0]);
 console.log(valores[1]);
 console.log(valores[2]);
 console.log(valores[3]);
 console.log(valores[4]);
 console.log(valores[5]);
-*/
 
 for (let pos = 0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem valor ${valores[pos]}`);
+}
+*/
+
+for (let pos in valores) {
+    console.log(`A poisição ${pos} tem valor ${valores[pos]}`);
 }
