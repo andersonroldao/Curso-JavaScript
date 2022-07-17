32c11a6b6bf55f0e71b90e9f44aeb9c52dc535b2
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-07-17 11:01:11 -0300

    Create ambiente-sort-push.js

---
+++ CursoJS/aulas/aula015-array-vetor/ambiente-sort-push.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3] // num é o vetor e [5, 8, 2, 9, 3] são os valores do vetor
+num.sort() // .sort() ordena os números em um vetor em ordem crescente.
+num.push(1) // .push() adiciona um valor à última posição em um vetor.
+console.log(num);
+console.log(`O vetor tem ${num.length} posições`); // .length mostra todos os valores no vetor
+console.log(`O primeiro valor do vetor é ${num[0]}`); // [0] é a posição do vetor
