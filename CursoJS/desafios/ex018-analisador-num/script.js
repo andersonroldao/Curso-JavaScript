481f6fcd4d28f77082d5976c114c3f5079fbc03a
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-07-19 14:45:57 -0300

    Update script.js

--- CursoJS/desafios/ex018-analisador-num/script.js
+++ CursoJS/desafios/ex018-analisador-num/script.js
@@ -1,15 +1,28 @@
-function adicionar(params) {
-    let num = document.getElementById('num')
-    let select = document.getElementById('select')
-    let n = Number(num.value)
+let select = document.querySelector('select#select')
+let res = document.querySelector('div#res')
+let valores = []
 
-    if (n <= 0 || n > 100) {
-        window.alert('Digite um número entre 1 e 100')
+function isNumero(n) {
+
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-
+        return false
     }
 }
 
-function finalizar(params) {
-    
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    let num = document.querySelector('input#num')
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+    } else {
+        window.alert('Digite um número entre 1 e 100')
+    }
 }
