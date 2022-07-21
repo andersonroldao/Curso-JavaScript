129643bae3ea18c3e0e301bc294906c93f4f8faf
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-07-21 13:20:01 -0300

    Update script.js

--- CursoJS/desafios/ex018-analisador-num/script.js
+++ CursoJS/desafios/ex018-analisador-num/script.js
@@ -1,9 +1,9 @@
-let select = document.querySelector('select#select')
-let res = document.querySelector('div#res')
+let num = document.getElementById('num')
+let lista = document.getElementById('select')
+let res = document.getElementById('res')
 let valores = []
 
 function isNumero(n) {
-
     if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
@@ -19,11 +19,29 @@ function inLista(n, l) {
     }
 }
 
-function adicionar() {
-    let num = document.querySelector('input#num')
-
+function adicionar(params) {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
+        let item = document.createElement('option')
+
+        select.appendChild(item)
+        valores.push(Number(num.value))
+        item.text = `Valor ${num.value} adicionado.`
+        res.innerHTML = ''
     } else {
-        window.alert('Digite um número entre 1 e 100')
+        alert('Valor inválido ou duplicado, tente novamente.\n\nDica: Tente um valor entre 1 e 100.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(params) {
+    let total = valores.length
+
+
+    if (valores.length == 0) {
+        alert('Digite um número antes de finalizar!')
+    } else {
+        res.innerHTML = ''
+        res.innerHTML += `A quantidade de números digitados foi: ${total}`
     }
 }
