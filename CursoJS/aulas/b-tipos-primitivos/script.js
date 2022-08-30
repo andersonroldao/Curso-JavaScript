a1ac4cad32bbec7ef587f7f67e86d188dfbba526
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-08-30 11:56:04 -0300

    Create script.js
    primitive types

---
+++ CursoJS/aulas/b-tipos-primitivos/script.js
@@ -0,0 +1,7 @@
+let nome = 'Anderson'; // String
+let idade = 25; // Number
+let sobrenome ; // Undefined
+let estaAprovado = true; // Boolean
+let corSelecionado = null; // Null
+
+console.log(nome, idade);
