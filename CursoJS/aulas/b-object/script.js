678d4cb94da384473ffb55e7df62f7eaf3334c58
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-08-30 11:53:39 -0300

    Create script.js
    small object

---
+++ CursoJS/aulas/b-object/script.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: 'Anderson',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: undefined
+};
+
+console.log(pessoa.nome, pessoa.idade, pessoa.estaAprovado, pessoa.sobrenome);
