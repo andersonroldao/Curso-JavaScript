f8e13586b766211d8bab835ddf3c358bbbaa6911
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-03 12:10:37 -0300

    Create logico.js

---
+++ CursoJS/aulas/b-operadores/logico.js
@@ -0,0 +1,20 @@
+/*
+e (&&),
+retorna TRUE quando os dois operadores forem true,
+retorna FALSE quando um dois operadores forem false,
+ou (||),
+retorna TRUE quando um dos dois operadores forem true,
+retorna FALSE quando os dois operadores forem false,
+não (!),
+retorna TRUE quando o operador for false;
+retorna FALSE quando o operador for true;
+*/
+
+console.log(true && true); // TRUE
+console.log(true && false); // FALSE
+
+console.log(true || true); // TRUE
+console.log(false || false); // FALSE
+
+console.log(! false); // TRUE
+console.log(! true); // FALSE
