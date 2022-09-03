32707b99264760bc48c7d1c5fcc5cdaee4f54f6b
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-03 12:10:32 -0300

    Create atribuicao.js

---
+++ CursoJS/aulas/b-operadores/atribuicao.js
@@ -0,0 +1,7 @@
+let valorProduto = 100;
+let valorProduto1 = 100;
+
+valorProduto += valorProduto; // valorProduto = valorProduto + valorProduto
+valorProduto1 -= valorProduto1; // valorProduto1 = valorProduto1 - valorProduto1
+console.log(valorProduto);
+console.log(valorProduto1);
