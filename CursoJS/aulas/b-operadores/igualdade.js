88dbe1ef40c682efdd753de087c14e5b98e02c3d
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-03 12:10:35 -0300

    Create igualdade.js

---
+++ CursoJS/aulas/b-operadores/igualdade.js
@@ -0,0 +1,7 @@
+// Igualdade estrita (forma recomendada)
+console.log(1 === 1); // true
+console.log('1' === 1); // false
+
+// Igualdade solta
+console.log(1 == 1); // true
+console.log('1' == 1); // true
