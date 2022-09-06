679b32e1b338be61b594329a010ebc7734ceb22d
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-06 11:53:32 -0300

    Delete if-else.js

--- CursoJS/aulas/b-if-else/if-else.js
+++
@@ -1,9 +0,0 @@
-let hora = data.getFullyear()
-
-if (hora >= 6 && hora <= 12) {
-    document.write(`Bom dia!`)
-} else if (hora > 12 && hora < 18) {
-    document.write(`Boa Tarde!`)
-} else {
-    document.write(`Boa Noite!`)
-}
