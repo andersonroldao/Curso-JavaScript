b7c80bda3219e34cae09fe99b7a25443dac31086
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-07 10:48:30 -0300

    Create while.js

---
+++ CursoJS/aulas/b-condicionais/while.js
@@ -0,0 +1,13 @@
+for (let i = 5; i >= 1; i--) { // conta os números impares de 5 a 1
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+let i = 5;
+while (i >= 1) { // conta os números impares de 5 a 1
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i--;
+}
