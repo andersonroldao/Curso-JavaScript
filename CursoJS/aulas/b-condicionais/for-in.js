69d5772aef8852d9538031b47e91fef9bc488b39
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-08 12:15:57 -0300

    Update for-in.js

--- CursoJS/aulas/b-condicionais/for-in.js
+++ CursoJS/aulas/b-condicionais/for-in.js
@@ -12,3 +12,12 @@ const cores = ['Vermelho', 'Azul', 'Verde'];
 for (const i in cores) {
     console.log(i, cores[i]);
 }
+
+const obj = {
+    "column01": "Coluna 01",
+    "column02": "Coluna 02"
+};
+
+for (const column in obj) {
+    console.log(column, '=', obj[column]);
+}
