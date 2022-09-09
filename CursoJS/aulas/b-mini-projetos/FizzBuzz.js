39698bee000dfde75133099925d0572309da3590
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-09 13:34:20 -0300

    Create FizzBuzz.js

---
+++ CursoJS/aulas/b-mini-projetos/FizzBuzz.js
@@ -0,0 +1,14 @@
+/*
+Divisível por 3 => Fizz
+Divisível por 5 => Buzz
+Divisível por 3 e 5 => FizzBuzz
+Não é divisível por 3 ou 5 => Num de entrada
+Não é um número => 'Não é um número'
+*/
+
+const resultado = FizzBuzz();
+console.log(resultado);
+
+function fizzBuzz(entrada) {
+    
+}
