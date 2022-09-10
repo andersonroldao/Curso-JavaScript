1caf7ec497e71bb83c9b877ef4e4904e8b660b57
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-10 12:15:19 -0300

    Update FizzBuzz.js

--- CursoJS/aulas/b-mini-projetos/FizzBuzz.js
+++ CursoJS/aulas/b-mini-projetos/FizzBuzz.js
@@ -5,10 +5,19 @@ Divisível por 3 e 5 => FizzBuzz
 Não é divisível por 3 ou 5 => Num de entrada
 Não é um número => 'Não é um número'
 */
-
-const resultado = FizzBuzz();
+const resultado = fizzBuzz(15);
 console.log(resultado);
 
 function fizzBuzz(entrada) {
-    
+    if (typeof entrada !== 'number') {
+        return 'Não é um número';
+    } else if (entrada % 3 === 0 && entrada % 5 === 0) {
+        return 'FizzBuzz';
+    } else if (entrada % 3 === 0) {
+        return 'Fizz';
+    } else if (entrada % 5 === 0) {
+        return 'Buzz';
+    } else {
+        return entrada;
+    }
 }
