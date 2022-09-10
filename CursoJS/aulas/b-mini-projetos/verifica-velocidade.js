f1ac5f7b111bd32a59b0e8c1885a1c4c1d6f79a3
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-10 12:25:56 -0300

    Create verifica-velocidade.js

---
+++ CursoJS/aulas/b-mini-projetos/verifica-velocidade.js
@@ -0,0 +1,9 @@
+// Velocidade máximo até 70km/h
+// A cada 5km/h acima do limite você ganha 1 ponto na carteira
+// Math.Floor()
+
+verificaVelocidade(75);
+
+function verificaVelocidade(velocidade) {
+
+}
