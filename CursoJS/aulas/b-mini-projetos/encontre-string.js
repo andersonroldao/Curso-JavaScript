b6db7e58c7c1b89eb4283de0467b41c6f54e388b
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-09-26 10:46:26 -0300

    Update encontre-string.js

--- CursoJS/aulas/b-mini-projetos/encontre-string.js
+++ CursoJS/aulas/b-mini-projetos/encontre-string.js
@@ -1,20 +1,20 @@
 /* Criar um método para ler propriedades de um objeto e exibir somente as propriedades do tipo string que estão nesse objeto */
 
 const filme = {
-    titulo: 'Vingadores',
-    ano: 2018,
-    diretor: 'Robin',
-    personagem: 'Thor'
+    titulo: 'Avengers',
+    ano: 2019,
+    diretor: 'Shaolin Matador de Porco',
+    personagem: 'Dorime'
 }
 
 exibirPropiedades(filme);
 
 function exibirPropiedades(obj) { // Exibe propriedades do tipo String.
-    for (const propriedades in filme) {
+    for (propriedades in obj) {
         if (typeof obj[propriedades] === 'string') {
             console.log(`${propriedades}: ${obj[propriedades]}`);
-        } else {
-            console.log(`${propriedades}: não é uma propriedades do tipo String!`);
-        }
+        } /* else {
+            console.log(`${propriedades}: Não é uma propriedades do tipo String!`);
+        } */
     }
 }
