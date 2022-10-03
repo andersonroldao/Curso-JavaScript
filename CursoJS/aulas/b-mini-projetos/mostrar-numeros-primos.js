0e67e7881b0f2307c7489e6e10e0e229c01f8bc0
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-10-03 12:17:30 -0300

    Create mostrar-numeros-primos.js

---
+++ CursoJS/aulas/b-mini-projetos/mostrar-numeros-primos.js
@@ -0,0 +1,18 @@
+// Função que mostra os números primos até o número desejado.
+
+exibirNumerosPrimos(37)
+function exibirNumerosPrimos(limite) {
+    for (let numero = 2; numero <= limite; numero++) {
+        let ehPrimo = true;
+
+        for (let divisor = 2; divisor < numero; divisor++) {
+            if (numero % divisor === 0) {
+                ehPrimo = false;
+                break;
+            }
+        }
+        if (ehPrimo) {
+            console.log(numero);
+        }
+    }
+}
