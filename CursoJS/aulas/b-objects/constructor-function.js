e6066cb6156280601690678c8cbcd11274033ff2
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-10-07 17:28:55 -0300

    Create constructor-function.js

---
+++ CursoJS/aulas/b-objects/constructor-function.js
@@ -0,0 +1,13 @@
+// PascalCase - UmDoisTresQuatro
+function Celular (marcaCelular, modeloCelular, tamanhoTela, CapacidadeBateria) {
+    this.marcaCelular = marcaCelular,
+    this.modeloCelular = modeloCelular,
+    this.tamanhoTela = tamanhoTela,
+    this.CapacidadeBateria = CapacidadeBateria
+    this.ligar = function ligar() {
+        console.log("Fazendo ligação...");
+    }
+}
+
+const celular = new Celular('Apple', 11, 6.1, 3110);
+console.log(celular);
