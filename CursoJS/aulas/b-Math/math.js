778b532e99ba3042e386d5f99e141f9c6322bb8f
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-10-07 17:28:48 -0300

    Create math.js

---
+++ CursoJS/aulas/b-Math/math.js
@@ -0,0 +1,13 @@
+// Escolhe o menor número e o maior
+min = Math.min(10, 5)
+max = Math.max(10, 5)
+
+console.log(min, max);
+
+// Gerando um número inteiro aleatório entre dois valores.
+
+/* function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomIntInclusive(0, 10)); */
