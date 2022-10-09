dae4566f685f5ebf92d2bc99623f680fec152e04
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-10-09 14:03:00 -0300

    Create template-literal.js

---
+++ CursoJS/aulas/b-string.js/template-literal.js
@@ -0,0 +1,20 @@
+const mensagem = 'Oi essa é a minha\n' + '\'primeira\' mensagem';
+console.log(mensagem);
+
+const mensagem1 = `Oi essa é minha
+'primeira' mensagem`;
+console.log(mensagem1);
+
+const nome = 'Fulano'
+const email =
+    `Olá ${nome} ${4 + 4 != 'oito'}
+
+blablabla, por blablabla,
+
+    contiue blabalbla,
+
+        Arigato,
+
+        Jão.`;
+
+console.log(email);
