9cf0d08b4effcd064c2636c2f02ec8151fe8d2ba
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-10-12 14:48:36 -0300

    Create montador-de-postagem.js

---
+++ CursoJS/desafios-b/montador-de-postagem.js
@@ -0,0 +1,15 @@
+// Objeto de postagem de blog
+
+let postagem = {
+    titulo: 'a',
+    mensagem: 'b',
+    autor: 'c',
+    visualizacoes: 10,
+    comentarios: [
+        { autor: 'a', mensagem: 'b' },
+        { autor: 'a', mensagem: 'b' }
+    ],
+    estaAoVivo: true
+}
+
+console.log(postagem);
