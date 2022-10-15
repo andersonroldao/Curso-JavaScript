49eabd80bb042e9599e616ac420626f9ff827279
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-10-15 14:29:35 -0300

    Create function01.js

---
+++ CursoJS/aulas/b-function/function01.js
@@ -0,0 +1,19 @@
+// Realiza uma tarefa e não retorna nada
+
+function dizNome() {
+    console.log('Anderson');
+}
+
+dizNome();
+
+// Faz um cálculo e retorna algo
+
+function multiplicaPorDois(valor) {
+    return valor * 2;
+}
+//console.log(multiplicaPorDois(5));
+
+
+let resultado = multiplicaPorDois(5);
+
+console.log(resultado);
