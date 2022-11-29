f80cf55abcbebab07261bff2af7bfd9ffb100970
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-11-29 20:41:15 -0300

    Create function01-par-imp.js

---
+++ CursoJS/aulas/aula016-function/a-function/function01-par-imp.js
@@ -0,0 +1,13 @@
+function parimp(num) {
+    if (num % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+console.log(parimp(11));
+
+/* or
+let res = parimp(11)
+console.log(res);
+*/
