80adac54637879e7bda38e56156f20ef91beb509
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-11-29 20:41:53 -0300

    Create join-split-slug.js

---
+++ CursoJS/aulas/aula016-function/b-arrays/join-split-slug.js
@@ -0,0 +1,14 @@
+const array1 = [1, 2, 3, 4, 5, 6];
+
+// Combinar
+const combinado = array1.join('.');
+console.log(combinado);
+
+// Dividir
+const frase = 'Mariline te espero na janta'
+const cortado = frase.split(' ');
+
+console.log(cortado);
+
+// Slug
+console.log(cortado.join('-'));
