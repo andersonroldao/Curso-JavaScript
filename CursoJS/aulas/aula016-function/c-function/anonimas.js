7868a09f20d11c503c0314b33f13277bb70cda40
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-11-30 20:56:42 -0300

    Create anonimas.js

---
+++ CursoJS/aulas/aula016-function/c-function/anonimas.js
@@ -0,0 +1,13 @@
+const subtraiDoisNumeros = function subtrai(a, b) {
+    return a - b;
+}
+
+console.log(subtraiDoisNumeros(10, 3));
+
+
+// Funções Anônimas - oculta o nome da function quando está dentro de uma variável
+const subtraiTresNumeros = function (a, b, c) {
+    return a - b - c;
+}
+
+console.log(subtraiTresNumeros(15, 3, 6));
