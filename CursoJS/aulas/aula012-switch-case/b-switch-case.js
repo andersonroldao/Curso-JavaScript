b019f2bc0c59320cbfc3598b41e19cdcd128587a
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-08 19:54:19 -0300

    Create b-switch-case.js

---
+++ CursoJS/aulas/aula012-switch-case/b-switch-case.js
@@ -0,0 +1,20 @@
+let permissao; // comum, gerente, diretor
+permissao = 'comum';
+
+switch (permissao) {
+    case 'comum':
+        console.log('Usuário comum');
+        break;
+
+    case 'gerente':
+        console.log('Usuário gerente');
+        break;
+
+    case 'diretor':
+        console.log('Usuário diretor');
+        break;
+
+    default:
+        console.log('Usuário não reconhecido!');
+        break;
+}
