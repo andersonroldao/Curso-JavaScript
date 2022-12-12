71b410d033e5eaeea8b010fff53dd95d5e13b29f
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-12 01:14:48 -0300

    Create pop.js

---
+++ CursoJS/aulas/aula015-array-vetor/c-arrays/pop.js
@@ -0,0 +1,7 @@
+const numeros = [5, 3, 12, 17, 10, 99];
+
+// Pop remove e retorna o último número do array
+const numeroRetornado = numeros.pop();
+
+console.log('Número Removido', numeroRetornado);
+console.log(numeros);
