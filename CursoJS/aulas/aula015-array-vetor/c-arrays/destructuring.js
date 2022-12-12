237cea039880bb071c7d202ee4e53679938f2424
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-12 01:14:27 -0300

    Create destructuring.js

---
+++ CursoJS/aulas/aula015-array-vetor/c-arrays/destructuring.js
@@ -0,0 +1,10 @@
+const user = [
+    'Anderson',
+    29,
+    'Programador',
+    2022,
+    'Brasileiro'
+];
+
+var [nome, idade, , ano, nacionalidade] = user;
+console.log(nome, idade, ano, nacionalidade);
