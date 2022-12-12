8d6774aa7925432afe45b8cbc2470c4d9ba7861e
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-12 01:14:33 -0300

    Create includes.js

---
+++ CursoJS/aulas/aula015-array-vetor/c-arrays/includes.js
@@ -0,0 +1,13 @@
+const user = [
+    'Anderson',
+    29,
+    2022,
+    'Brasileiro',
+    'RS'
+];
+
+
+// Includes retorna se o valor está no array 
+console.log(user.includes('Anderson'));
+console.log(user.includes('Japão'));
+console.log(user.includes(2022));
