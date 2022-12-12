f4a64ec0716bcc1ea7a9bccd274499a7a8683bb6
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-12 01:14:45 -0300

    Create join.js

---
+++ CursoJS/aulas/aula015-array-vetor/c-arrays/join.js
@@ -0,0 +1,15 @@
+const numeros = [5, 3, 12, 17, 10, 99];
+
+// Join junta os valores do array
+console.log(numeros.join('-'));
+
+const user = [
+    'Anderson',
+    29,
+    2022,
+    'Brasileiro'
+];
+
+console.log(user);
+console.log(user.join(', '));
+console.log(user.join('-'));
