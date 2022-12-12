8f6b7c81c05e05b1744a349409fe055701b7e051
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-12 01:14:39 -0300

    Create indexOf.js

---
+++ CursoJS/aulas/aula015-array-vetor/c-arrays/indexOf.js
@@ -0,0 +1,13 @@
+const user = [
+    'Anderson',
+    29,
+    2022,
+    'Brasileiro',
+    'RS'
+];
+
+
+// IndexOf retorna se o valor está no array e a posição dele (retorna -1 não estiver no array)
+console.log(user.indexOf('Anderson'));
+console.log(user.indexOf('Japão'));
+console.log(user.indexOf(2022));
