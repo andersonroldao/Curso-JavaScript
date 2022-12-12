36a60d1141c57df0fbb904ec9a1094ca8c964484
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-12 01:14:24 -0300

    Create concat.js

---
+++ CursoJS/aulas/aula015-array-vetor/c-arrays/concat.js
@@ -0,0 +1,6 @@
+const arr1 = ['Programador', 'a', 'Bordo'];
+const arr2 = [3, 2, 1];
+
+// Concat unifica valores de arrays
+const res = [arr1.concat(arr2).concat(['fulano', 'ciclano', 'beltrano'])];
+console.log(res);
