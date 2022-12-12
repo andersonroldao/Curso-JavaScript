bfc7ac82a3d7cea4bdd919def5256c3ce0bae2ef
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2022-12-12 01:14:51 -0300

    Create push.js

---
+++ CursoJS/aulas/aula015-array-vetor/c-arrays/push.js
@@ -0,0 +1,14 @@
+const user = [
+    'Anderson',
+    29,
+    2022,
+    'Brasileiro'
+];
+
+
+// Push adiciona a última posição do array
+console.log(user);
+
+user.push('RS');
+
+console.log(user);
