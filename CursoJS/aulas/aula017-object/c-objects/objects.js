d65d6acbcfb88811c8f87ffef686da82f1b0e2d3
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2023-08-14 12:29:32 -0300

    Create objects.js

---
+++ CursoJS/aulas/aula017-object/c-objects/objects.js
@@ -0,0 +1,10 @@
+const user = {
+    nome: 'Anderson',
+    idade: 26,
+    ano: 2022,
+    'ultimo-nome': 'Silva'
+}
+
+console.log(user);
+console.log(user.nome, user.idade, user.ano);
+console.log(user.nome, user.idade, user.ano, user['ultimo-nome']);
